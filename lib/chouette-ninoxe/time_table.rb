e1937c00666352d3cd40f21e4912600f7fc8a912
Marc Florisson <marc.florisson@dryade.net>
2011-07-20 14:34:07 +0200

    add validity period on TimeTable. Refs #173

--- lib/chouette-ninoxe/time_table.rb
+++ lib/chouette-ninoxe/time_table.rb
@@ -1,5 +1,13 @@
 class Chouette::TimeTable < Chouette::ActiveRecord
   has_many :dates, :class_name => "Chouette::TimeTableDate", :order => :position
   has_many :periods, :class_name => "Chouette::TimeTablePeriod", :order => :position
+
+  def self.start_validity_period
+    ( Chouette::TimeTableDate.all.map(&:date) + Chouette::TimeTablePeriod.all.map(&:periodstart)).min
+  end
+  def self.end_validity_period
+    ( Chouette::TimeTableDate.all.map(&:date) + Chouette::TimeTablePeriod.all.map(&:periodend)).max
+  end
+
 end
 
