9466bab6915e2335407510600fafc542448ef7c9
Alban Peignier <alban@tryphon.eu>
2012-03-11 14:56:59 +0100

    Add Chouette::ActiveRecord#human_attribute_name alias

--- lib/chouette-ninoxe/active_record.rb
+++ lib/chouette-ninoxe/active_record.rb
@@ -7,6 +7,10 @@ class Chouette::ActiveRecord < ::ActiveRecord::Base
 
   self.abstract_class = true
 
+  def human_attribute_name(*args)
+    self.class.human_attribute_name(*args)
+  end
+
   def self.compute_table_name
     Inflector.chouettify super.singularize
   end
