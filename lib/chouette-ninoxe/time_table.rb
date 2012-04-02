ca8b1a91083148ec4b195544c3a17b81cbbe7f7d
Michel Etienne <michel.etienne@dryade.net>
2012-04-02 16:43:51 +0200

    timetable with daytypes flags

--- lib/chouette-ninoxe/time_table.rb
+++ lib/chouette-ninoxe/time_table.rb
@@ -9,5 +9,61 @@ class Chouette::TimeTable < Chouette::ActiveRecord
     ( Chouette::TimeTableDate.all.map(&:date) + Chouette::TimeTablePeriod.all.map(&:periodend)).max
   end
 
+  def day_by_mask(flag)
+    int_day_types & flag == flag
+  end
+
+  def monday
+    day_by_mask(4)
+  end
+  def tuesday
+    day_by_mask(8)
+  end
+  def wednesday
+    day_by_mask(16)
+  end
+  def thursday
+    day_by_mask(32)
+  end
+  def friday
+    day_by_mask(64)
+  end
+  def saturday
+    day_by_mask(128)
+  end
+  def sunday
+    day_by_mask(256)
+  end
+  
+  def set_day(day,flag)
+    if (day) 
+      self.int_day_types |= flag
+    else
+      self.int_day_types ^= flag
+    end
+  end
+  
+  def monday=(day)
+    set_day(day,4)
+  end
+  def tuesday=(day)
+    set_day(day,8)
+  end
+  def wednesday=(day)
+    set_day(day,16)
+  end
+  def thursday=(day)
+    set_day(day,32)
+  end
+  def friday=(day)
+    set_day(day,64)
+  end
+  def saturday=(day)
+    set_day(day,128)
+  end
+  def sunday=(day)
+    set_day(day,256)
+  end
+  
 end
 
