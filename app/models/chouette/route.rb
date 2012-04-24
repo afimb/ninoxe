dd37c54f23946dd4eec356ce761190f2b66ff247
Marc Florisson <marc.florisson@dryade.net>
2012-04-24 13:20:58 +0200

    add direction enum on route model

--- app/models/chouette/route.rb
+++ app/models/chouette/route.rb
@@ -3,6 +3,7 @@ class Chouette::Route < Chouette::ActiveRecord
   set_primary_key :id
 
   attr_accessor :wayback_code
+  attr_accessor :direction_code
 
   belongs_to :line
   has_many :stop_points, :order => 'position', :dependent => :destroy
@@ -52,23 +53,54 @@ class Chouette::Route < Chouette::ActiveRecord
     [:creationtime]
   end
 
+  def self.direction_binding
+    { "A" => "straight_forward", 
+      "R" => "backward",
+      "CLOCKWISE" => "clockwise",
+      "ANTICLOCKWISE" => "anticlockwise",
+      "NORTH" => "north",
+      "NORTH_WEST" => "north_west", 
+      "WEST" => "west",
+      "SOUTH_WEST" => "south_west",
+      "SOUTH" => "south",
+      "SOUTH_EAST" => "south_east",
+      "EAST" => "east",
+      "NORTH_EAST" => "north_east"}
+  end
+  def direction_code
+    return nil if self.class.direction_binding[direction].nil?
+    Chouette::Direction.new( self.class.direction_binding[direction])
+  end
+  def direction_code=(direction_code)
+    self.direction = nil
+    self.class.direction_binding.each do |k,v| 
+      self.direction = k if v==direction_code
+    end
+  end
+  @@directions = nil
+  def self.directions
+    @@directions ||= Chouette::Direction.all
+  end
   def self.wayback_binding
     { "A" => "straight_forward", "R" => "backward"}
   end
   def wayback_code
+    return nil if self.class.wayback_binding[wayback].nil?
     Chouette::Wayback.new( self.class.wayback_binding[wayback])
   end
-
   def wayback_code=(wayback_code)
     self.wayback = nil
     self.class.wayback_binding.each do |k,v| 
       self.wayback = k if v==wayback_code
     end
   end
-
   @@waybacks = nil
   def self.waybacks
     @@waybacks ||= Chouette::Wayback.all
+  end
+  
+  def stop_areas
+    Chouette::StopArea.joins(:stop_points => :route).where(:route => {:id => self.id})
   end
 end
 
