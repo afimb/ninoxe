0f2f0ab0d030c395464f43e4d5da79f5b74a89c6
Michel Etienne <michel.etienne@dryade.net>
2012-05-14 09:09:23 +0200

    add time_table_period specs

--- app/models/chouette/time_table_date.rb
+++ app/models/chouette/time_table_date.rb
@@ -2,7 +2,7 @@ class Chouette::TimeTableDate < Chouette::ActiveRecord
   set_primary_keys :timetableid, :position
   belongs_to :time_table
   acts_as_list :scope => 'timetableid = #{time_table_id}',:top_of_list => 0
-
+  
   def self.model_name
     ActiveModel::Name.new Chouette::TimeTableDate, Chouette, "TimeTableDate"
   end
--- app/models/chouette/time_table_period.rb
+++ app/models/chouette/time_table_period.rb
@@ -1,7 +1,8 @@
-#require 'composite_primary_keys'
-
 class Chouette::TimeTablePeriod < Chouette::ActiveRecord
-  #set_primary_keys :timetableid, :position
+  set_primary_keys :timetableid, :position
   belongs_to :time_table
   acts_as_list :scope => 'timetableid = #{time_table_id}',:top_of_list => 0
+  def self.model_name
+    ActiveModel::Name.new Chouette::TimeTablePeriod, Chouette, "TimeTablePeriod"
+  end
 end
