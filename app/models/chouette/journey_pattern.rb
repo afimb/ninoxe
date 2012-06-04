309a0bbec119e9ec57acb6cefa5b2b913278cd3b
Marc Florisson <marc.florisson@dryade.net>
2012-06-04 08:50:09 +0200

    fix migrations

--- app/models/chouette/journey_pattern.rb
+++ app/models/chouette/journey_pattern.rb
@@ -6,7 +6,8 @@ class Chouette::JourneyPattern < Chouette::TridentActiveRecord
   has_many :vehicle_journeys, :dependent => :destroy
   has_many :vehicle_journey_at_stops, :through => :vehicle_journeys
   has_and_belongs_to_many :stop_points, :class_name => 'Chouette::StopPoint', :order => 'stop_points.position', :before_add => :vjas_add, :before_remove => :vjas_remove, :after_add => :shortcuts_update, :after_remove => :shortcuts_update
-  attr_accessible :route_id, :objectid, :object_version, :creation_time, :creator_id, :name, :comment, :registration_number, :published_name, :departure_stop_point_id, :arrival_stop_point_id
+
+  attr_accessible :route_id, :objectid, :object_version, :creation_time, :creator_id, :name, :comment, :registration_number, :published_name, :departure_stop_point_id, :arrival_stop_point_id, :stop_point_ids
   
   def departure_stop_point
     return unless departure_stop_point_id
