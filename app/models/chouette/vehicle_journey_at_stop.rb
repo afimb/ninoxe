f75aaa8ba65bc798d663a2d8b9b4c2bd45986fce
Michel Etienne <metienne@cityway.fr>
2012-07-10 10:16:28 +0200

    add visibility on fields

--- app/models/chouette/vehicle_journey_at_stop.rb
+++ app/models/chouette/vehicle_journey_at_stop.rb
@@ -1,10 +1,13 @@
 class Chouette::VehicleJourneyAtStop < Chouette::ActiveRecord
+  set_primary_key :id
+  
   belongs_to :stop_point
   belongs_to :vehicle_journey
 
   after_initialize :set_virtual_attributes
 
   attr_accessor :_destroy
+  attr_accessible :position, :is_departure, :is_arrival
   attr_accessible :vehicle_journey_id, :stop_point_id, :connecting_service_id, :boarding_alighting_possibility, :arrival_time, :departure_time, :waiting_time, :elapse_duration, :headway_frequency, :_destroy, :stop_point
 
   def increasing_times_validate( previous)
