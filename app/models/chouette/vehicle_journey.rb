a11f513671e1d5c31534a0bc989aaad71d3f8cca
Marc Florisson <marc.florisson@dryade.net>
2012-08-14 17:26:05 +0200

    add attr_accessible on vehicle_journey#route

--- app/models/chouette/vehicle_journey.rb
+++ app/models/chouette/vehicle_journey.rb
@@ -4,7 +4,7 @@ class Chouette::VehicleJourney < Chouette::TridentActiveRecord
   set_primary_key :id
 
   attr_accessible :route_id, :journey_pattern_id, :time_slot_id, :company_id, :objectid, :object_version, :creation_time, :creator_id, :comment, :status_value
-  attr_accessible :transport_mode, :published_journey_name, :published_journey_identifier, :facility, :vehicle_type_identifier, :number
+  attr_accessible :route, :transport_mode, :published_journey_name, :published_journey_identifier, :facility, :vehicle_type_identifier, :number
   attr_accessible :vehicle_journey_at_stops_attributes, :time_table_tokens, :time_tables
   attr_reader :time_table_tokens
 
