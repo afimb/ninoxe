3a1b90a5954bdf262d257e7e9ba1d13d27a1a95e
Marc Florisson <marc.florisson@dryade.net>
2012-11-30 11:03:20 +0100

    move explict postgresadapter require in dummy app

--- lib/ninoxe.rb
+++ lib/ninoxe.rb
@@ -1,7 +1,6 @@
 require "ninoxe/engine"
 
 require "acts_as_list"
-require 'active_record/connection_adapters/postgresql_adapter'
 #require "composite_primary_keys"
 
 module Ninoxe
--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+require 'active_record/connection_adapters/postgresql_adapter'
+
 # Initialize the rails application
 Dummy::Application.initialize!
