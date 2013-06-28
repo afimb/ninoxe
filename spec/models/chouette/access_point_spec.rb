67fb0ea048c89f036a88e37dee9f1ecd0c2249c1
Michel Etienne <metienne@cityway.fr>
2013-06-28 14:57:50 +0200

    remove projection attributes from stop areas and access points

--- spec/models/chouette/access_point_spec.rb
+++ spec/models/chouette/access_point_spec.rb
@@ -7,35 +7,6 @@ describe Chouette::AccessPoint do
   it { should validate_presence_of :name }
   it { should validate_numericality_of :latitude }
   it { should validate_numericality_of :longitude }
-  it { should validate_numericality_of :x }
-  it { should validate_numericality_of :y }
-
-  describe ".x_y" do
-    it "should accept x and y both as nil" do
-      subject = Factory :access_point
-      subject.x = nil
-      subject.y = nil
-      subject.valid?.should be_true
-    end
-    it "should accept x and y both numerical" do
-      subject = Factory :access_point
-      subject.x = 10
-      subject.y = 10
-      subject.valid?.should be_true
-    end
-    it "should reject x nil with y numerical" do
-      subject = Factory :access_point
-      subject.x = nil
-      subject.y = 10
-      subject.valid?.should be_false
-    end
-    it "should reject x numerical with y nil" do
-      subject = Factory :access_point
-      subject.x = 10
-      subject.y = nil
-      subject.valid?.should be_false
-    end
-  end 
   
   describe ".latitude" do
     it "should accept -90 value" do
