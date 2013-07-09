6854323b55fb7917aa4da520caa6198d130ee6c2
Luc Donnet <luc.donnet@dryade.net>
2013-07-09 13:25:40 +0200

    Take stop areas with geometry for route presenter

--- app/presenters/chouette/geometry/route_presenter.rb
+++ app/presenters/chouette/geometry/route_presenter.rb
@@ -8,13 +8,13 @@ class Chouette::Geometry::RoutePresenter
   # return route's stop_areas cloud geometry
   #
   def stop_areas_geometry
-    to_multi_point_feature( @route.stop_areas)
+    to_multi_point_feature( @route.stop_areas.with_geometry )
   end
 
   # return route geometry based on BoardingPosition or Quay
   #
   def geometry
-    to_line_string_feature( @route.stop_areas)
+    to_line_string_feature( @route.stop_areas.with_geometry )
   end
 
 
