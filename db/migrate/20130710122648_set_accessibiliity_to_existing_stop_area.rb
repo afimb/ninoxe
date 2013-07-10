ae8291fe90073bf5f00fe26526c3aa0abc0b602b
Michel Etienne <metienne@cityway.fr>
2013-07-10 14:39:39 +0200

    initialize values on new stopArea fields

---
+++ db/migrate/20130710122648_set_accessibiliity_to_existing_stop_area.rb
@@ -0,0 +1,16 @@
+class SetAccessibiliityToExistingStopArea < ActiveRecord::Migration
+  def up
+    Chouette::StopArea.all.each do |s|
+      if s.mobility_restricted_suitability.nil?
+        s.mobility_restricted_suitability = false
+        s.stairs_availability = false
+        s.lift_availability = false
+        s.int_user_needs = 0
+        s.save
+      end
+    end
+  end
+
+  def down
+  end
+end
