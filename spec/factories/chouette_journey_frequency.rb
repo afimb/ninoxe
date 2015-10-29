009c77903bd3d9b0a6af48e49ef2f420274a7451
Bruno Perles <bruno@atnos.com>
2015-10-29 11:48:48 +0100

    Add spec for journey_frequency

---
+++ spec/factories/chouette_journey_frequency.rb
@@ -0,0 +1,27 @@
+FactoryGirl.define do
+
+  factory :journey_frequency, class: Chouette::JourneyFrequency do
+    vehicle_journey
+    timeband
+    scheduled_headway_interval { Time.now }
+    first_departure_time { timeband.start_time }
+    last_departure_time { timeband.end_time }
+  end
+
+  factory :journey_frequency_first_departure_time_invalid, class: Chouette::JourneyFrequency do
+    vehicle_journey
+    timeband
+    scheduled_headway_interval { Time.now }
+    first_departure_time { timeband.start_time - 1.minute }
+    last_departure_time { timeband.end_time }
+  end
+
+  factory :journey_frequency_last_departure_time_invalid, class: Chouette::JourneyFrequency do
+    vehicle_journey
+    timeband
+    scheduled_headway_interval { Time.now }
+    first_departure_time { timeband.start_time }
+    last_departure_time { timeband.end_time + 1.minute }
+  end
+
+end
--- spec/models/chouette/journey_frequency_spec.rb
+++ spec/models/chouette/journey_frequency_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
-describe Chouette::JourneyFrequency, :type => :model do
+describe Chouette::JourneyFrequency, type: :model do
+  describe '#create' do
+    context 'when valid' do
+      it { create(:journey_frequency) }
+    end
 
+    context 'when first_departure_time not valid' do
+      it 'fails validation with first_departure_time before timeband start_time' do
+        journey_frequency = build(:journey_frequency_first_departure_time_invalid)
+        expect(journey_frequency).to be_invalid
+      end
+    end
+
+    context 'when last_departure_time not valid' do
+      it 'fails validation with last_departure_time after timeband end_time' do
+        journey_frequency = build(:journey_frequency_last_departure_time_invalid)
+        expect(journey_frequency).to be_invalid
+      end
+    end
+  end
 end
