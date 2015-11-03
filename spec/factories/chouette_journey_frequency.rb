60f876d8abaef307ed56b1a76b7223b84728bc50
Bruno Perles <bruno@atnos.com>
2015-11-03 16:01:10 +0100

    Add spec for journey_frequency

--- spec/factories/chouette_journey_frequency.rb
+++ spec/factories/chouette_journey_frequency.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :journey_frequency, class: Chouette::JourneyFrequency do
     vehicle_journey_id { 1 }
     timeband
-    scheduled_headway_interval { Time.now }
+    scheduled_headway_interval { Time.current }
     first_departure_time { timeband.start_time }
     last_departure_time { timeband.end_time }
   end
@@ -11,7 +11,7 @@ FactoryGirl.define do
   factory :journey_frequency_first_departure_time_invalid, class: Chouette::JourneyFrequency do
     vehicle_journey_id { 1 }
     timeband
-    scheduled_headway_interval { Time.now }
+    scheduled_headway_interval { Time.current }
     first_departure_time { timeband.start_time - 1.minute }
     last_departure_time { timeband.end_time }
   end
@@ -19,9 +19,25 @@ FactoryGirl.define do
   factory :journey_frequency_last_departure_time_invalid, class: Chouette::JourneyFrequency do
     vehicle_journey_id { 1 }
     timeband
-    scheduled_headway_interval { Time.now }
+    scheduled_headway_interval { Time.current }
     first_departure_time { timeband.start_time }
     last_departure_time { timeband.end_time + 1.minute }
+  end
+
+  factory :journey_frequency_departure_time_invalid, class: Chouette::JourneyFrequency do
+    vehicle_journey_id { 1 }
+    timeband
+    scheduled_headway_interval { Time.current }
+    first_departure_time { '00:00' }
+    last_departure_time { '00:00' }
+  end
+
+  factory :journey_frequency_scheduled_headway_interval_invalid, class: Chouette::JourneyFrequency do
+    vehicle_journey_id { 1 }
+    timeband
+    scheduled_headway_interval { '00:00' }
+    first_departure_time { timeband.start_time }
+    last_departure_time { timeband.end_time }
   end
 
 end
--- spec/models/chouette/journey_frequency_spec.rb
+++ spec/models/chouette/journey_frequency_spec.rb
@@ -19,5 +19,19 @@ describe Chouette::JourneyFrequency, type: :model do
         expect(journey_frequency).to be_invalid
       end
     end
+
+    context 'when first and last departure_time not valid' do
+      it 'fails validation with first_departure_time equal last_departure_time' do
+        journey_frequency = build(:journey_frequency_departure_time_invalid)
+        expect(journey_frequency).to be_invalid
+      end
+    end
+
+    context 'when scheduled_headway_interval not valid' do
+      it 'fails validation with scheduled_headway_interval is not set' do
+        journey_frequency = build(:journey_frequency_scheduled_headway_interval_invalid)
+        expect(journey_frequency).to be_invalid
+      end
+    end
   end
 end
