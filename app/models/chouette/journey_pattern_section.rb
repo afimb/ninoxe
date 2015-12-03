63a7c4068f15b818a8ff8f06bbf07e9460f774ce
Bruno Perles <bruno@atnos.com>
2015-12-03 16:10:24 +0100

    Add method to create by rank

--- app/models/chouette/journey_pattern_section.rb
+++ app/models/chouette/journey_pattern_section.rb
@@ -6,4 +6,13 @@ class Chouette::JourneyPatternSection < Chouette::ActiveRecord
   validates :route_section_id, presence: true
   validates :rank, presence: true, numericality: :only_integer
   validates :journey_pattern_id, uniqueness: { scope: [:route_section_id, :rank] }
+
+  def self.update_by_journey_pattern_rank(journey_pattern_id, route_section_id, rank)
+    jps = self.find_or_initialize_by(journey_pattern_id: journey_pattern_id, rank: rank)
+    if route_section_id.present?
+      jps.update_attributes(route_section_id: route_section_id)
+    else
+      jps.destroy
+    end
+  end
 end
