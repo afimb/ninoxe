7f6afe6ebaa1b9f8f3462fff01ff1bf1bbf0433c
Bruno Perles <bruno@atnos.com>
2015-12-07 15:39:31 +0100

    Add control to journey_pattern_section

--- app/models/chouette/journey_pattern_section.rb
+++ app/models/chouette/journey_pattern_section.rb
@@ -7,6 +7,8 @@ class Chouette::JourneyPatternSection < Chouette::ActiveRecord
   validates :rank, presence: true, numericality: :only_integer
   validates :journey_pattern_id, uniqueness: { scope: [:route_section_id, :rank] }
 
+  default_scope { order(:rank) }
+
   def self.update_by_journey_pattern_rank(journey_pattern_id, route_section_id, rank)
     jps = self.find_or_initialize_by(journey_pattern_id: journey_pattern_id, rank: rank)
     if route_section_id.present?
