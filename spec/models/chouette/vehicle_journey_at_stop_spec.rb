f62cdb7193a31243ba1336cfcb8d2e60e13fa9de
Bruno Perles <bruno@atnos.com>
2016-01-07 10:08:24 +0100

    Update spec

--- spec/models/chouette/vehicle_journey_at_stop_spec.rb
+++ spec/models/chouette/vehicle_journey_at_stop_spec.rb
@@ -8,7 +8,7 @@ describe Chouette::VehicleJourneyAtStop, :type => :model do
   describe "#exceeds_gap?" do
     it "should return false if gap < 1.hour" do
       t1 = 1.minutes.ago
-      t2 = 1.minutes.ago + 3.hour
+      t2 = 1.minutes.ago + 4.hour
       expect(subject.exceeds_gap?(t1, t2)).to be_truthy
     end
     it "should return false if gap > 2.hour" do
@@ -23,7 +23,7 @@ describe Chouette::VehicleJourneyAtStop, :type => :model do
     let(:vjas2){ vehicle_journey.vehicle_journey_at_stops[1]}
     context "when vjas#arrival_time exceeds gap" do
       it "should add errors on arrival_time" do
-        vjas1.arrival_time = vjas2.arrival_time - 3.hour
+        vjas1.arrival_time = vjas2.arrival_time - 5.hour
         expect(vjas2.increasing_times_validate(vjas1)).to be_falsey
         expect(vjas2.errors).not_to be_empty
         expect(vjas2.errors[:arrival_time]).not_to be_blank
@@ -31,7 +31,7 @@ describe Chouette::VehicleJourneyAtStop, :type => :model do
     end
     context "when vjas#departure_time exceeds gap" do
       it "should add errors on departure_time" do
-        vjas1.departure_time = vjas2.departure_time - 3.hour
+        vjas1.departure_time = vjas2.departure_time - 5.hour
         expect(vjas2.increasing_times_validate(vjas1)).to be_falsey
         expect(vjas2.errors).not_to be_empty
         expect(vjas2.errors[:departure_time]).not_to be_blank
